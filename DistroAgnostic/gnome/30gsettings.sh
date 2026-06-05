6c6d81556480d5676cc21039c5797c803e701e43
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-06-05 19:36:42 +0200

    Ubuntu 26.04 migration fixes

--- DistroAgnostic/gnome/30gsettings.sh
+++ DistroAgnostic/gnome/30gsettings.sh
@@ -51,48 +51,36 @@ for_each "gsettings set org.gnome.shell.app-switcher " << 'BASH'
     current-workspace-only "true"
 BASH
 
-# Ptyxis and accent-color are not present on Ubuntu 24.04 - check and skip
-# TODO remove the ifdef after Ubuntu 26.04 is avaliable is GitHub Actions runner
-if gsettings get org.gnome.desktop.interface accent-color; then
-
-    for_each "gsettings set org.gnome.Ptyxis " << 'BASH'
-        restore-session "false"
-        restore-window-size "false"
-        scrollbar-policy "always"
+for_each "gsettings set org.gnome.desktop.calendar " << 'BASH'
+    show-weekdate "true"
 BASH
 
-    for_each "gsettings set org.gnome.desktop.calendar " << 'BASH'
-        show-weekdate "true"
+for_each "gsettings set org.gnome.desktop.input-sources " << 'BASH'
+    per-window true
+    sources "[('xkb', 'us'), ('xkb', 'sk+qwerty')]"
+    xkb-options "['terminate:ctrl_alt_bksp', 'shift:both_capslock', 'caps:escape']"
 BASH
 
-    for_each "gsettings set org.gnome.desktop.input-sources " << 'BASH'
-        per-window true
-        sources "[('xkb', 'us'), ('xkb', 'sk+qwerty')]"
-        xkb-options "['terminate:ctrl_alt_bksp', 'shift:both_capslock', 'caps:escape']"
+for_each "gsettings set org.gnome.desktop.interface " << 'BASH'
+    # WARNING: text-scaling-factor can break Chromium (or other GTK apps) and cause
+    # the app window to increase in size on refocus (due to floating point rounding
+    # errors). Always set the value to decimal that can be represented as float without
+    # rounding error and also test whether the Chromium window does not change size on
+    # refocus with the specific value.
+    # TODO consider using 1 as scaling factor and setting interface/document/monospace fonts instead
+    text-scaling-factor "1"
+    # text-scaling-factor '1.1875' # this should also work - 1.0011 in binary
+    font-name "Ubuntu Sans 14"
+    document-font-name "Sans 13"
+    monospace-font-name "Ubuntu Sans Mono 15"
+
+    font-hinting "slight"
+    cursor-size "64"
+
+    enable-animations "false"
+
+    color-scheme "prefer-dark"
+    gtk-theme "Yaru-red-dark"
+    icon-theme "Yaru-red-dark"
 BASH
 
-    for_each "gsettings set org.gnome.desktop.interface " << 'BASH'
-        # WARNING: text-scaling-factor can break Chromium (or other GTK apps) and cause
-        # the app window to increase in size on refocus (due to floating point rounding
-        # errors). Always set the value to decimal that can be represented as float without
-        # rounding error and also test whether the Chromium window does not change size on
-        # refocus with the specific value.
-        # TODO consider using 1 as scaling factor and setting interface/document/monospace fonts instead
-        text-scaling-factor "1"
-        # text-scaling-factor '1.1875' # this should also work - 1.0011 in binary
-        font-name "Ubuntu Sans 14"
-        document-font-name "Sans 13"
-        monospace-font-name "Ubuntu Sans Mono 15"
-
-        font-hinting "slight"
-        cursor-size "64"
-
-        enable-animations "false"
-
-        accent-color "red"
-        color-scheme "prefer-dark"
-        gtk-theme "Yaru-red-dark"
-        icon-theme "Yaru-red-dark"
-BASH
-
-fi # Ubuntu 24.04
